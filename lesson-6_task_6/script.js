860fddaa05c93b90d4681fec04bc5dfacbecd1f0
sergey.makarov <sergey.makarov@maxidom.ru>
2021-06-18 01:59:55 +0300

    верстка

--- lesson-6_task_6/script.js
+++ lesson-6_task_6/script.js
@@ -14,25 +14,5 @@ items = [
         name: 'Футболка',
         price: '800',
         pic: 'img/t-shirt.jpg',
-    }
+    },
 ];
-
-console.log(items);
-
-var catalog = document.getElementById('js-items');
-
-bypassDOM(catalog.children);
-
-function bypassDOM(pointIn){
-    for(i = 0; pointIn.length >= i; i++){
-        if(pointIn.className != 'js-pic'){
-            console.log('js-pic не найден');
-            // console.log(pointIn[i].children[i].children[i].className);
-            if(i == pointIn.length){
-                bypassDOM(pointIn[i].children[i].children[i]);
-            }
-        }
-    }
-}
-
-console.log(catalog.children);
